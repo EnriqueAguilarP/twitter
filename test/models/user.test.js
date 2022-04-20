2b2f7cbac6aaf34008444cc625875ba874406722
EnriqueAguilarP <dialectikos@gmail.com>
2022-04-20 09:54:01 -0500

    Requerimiento 2: datos de tipo fecha

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -2,12 +2,12 @@ const User = require('./../../app/models/user')
 
 describe("Unit test for user class", () => {
     test('Create an user object', () => {
-      const enriqueAguilar = new User(301578, "enriqueAguilarP", "Enrique Aguilar", "Javascript developer", "September 2021", "April 20 2022")
+      const enriqueAguilar = new User(301578, "enriqueAguilarP", "Enrique Aguilar", "Javascript developer")
       expect(enriqueAguilar.id).toBe(301578)
       expect(enriqueAguilar.username).toBe("enriqueAguilarP")
       expect(enriqueAguilar.name).toBe("Enrique Aguilar")
       expect(enriqueAguilar.bio).toBe("Javascript developer")
-      expect(enriqueAguilar.dateCreated).toBe("September 2021")
-      expect(enriqueAguilar.lastUpdated).toBe("April 20 2022")
+      expect(enriqueAguilar.dateCreated).not.toBeUndefined()
+      expect(enriqueAguilar.lastUpdated).not.toBeUndefined()
     });
   })
